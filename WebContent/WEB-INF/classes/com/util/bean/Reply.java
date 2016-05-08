4d3bac76839a46ae86c066069022346359ca1c52
LiangY <liangyi19941213@gmail.com>
2016-05-08 22:49:40 +0200

    Added files via upload

---
+++ WebContent/WEB-INF/classes/com/util/bean/Reply.java
@@ -0,0 +1,9 @@
+package com.util.bean;
+
+public class Reply {
+	private int replyID;
+	private String replyDate;
+	private String replyContent;
+	private int messageID;
+	private int employeeID;
+}
